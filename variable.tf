b8bb2c4a0774054f351f6354e5d506a2584a4e0b
jnssk <97682964+jnssk@users.noreply.github.com>
2022-02-02 11:02:19 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,8 +1,8 @@
 variable "aws_access_key" {
-  default = "AKIA37SMA5K2N5QRFINB"
+  default = "paste aws access keys"
 }
 variable "aws_secret_key" {
-  default = "45Ss9ADCi1oGTWq+BhQzDrVk+DZhLADmsymrwPAL"
+  default = "aws secret key"
 }
 variable "region" {
   default = "us-east-1"
