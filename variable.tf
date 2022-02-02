ca4c4a98e79df7db1c60c3ee9eed64762927b189
jnssk <97682964+jnssk@users.noreply.github.com>
2022-02-02 10:57:58 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,10 @@
+variable "aws_access_key" {
+  default = "AKIA37SMA5K2N5QRFINB"
+}
+variable "aws_secret_key" {
+  default = "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"
+}
+variable "region" {
+  default = "us-east-1"
+
+}
